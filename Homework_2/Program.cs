7fdd9291a19837309e7c7fc44b1b08dd06e0b48f
Asya <fcz777@mail.ru>
2022-06-29 16:13:16 +0300

    First task added

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = number / 10 % 10;
+if(number / 10 <= 9){
+    Console.WriteLine("Неверное число");
+}
+else{
+    Console.WriteLine("-> " + secondDigit);
+}
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+// int number = new Random().Next(10,100);
+// // Console.Write(number);
+// int num1 = number / 10;
+// int num2 = number % 10;
+// Console.WriteLine(number + " " + num1 + " " + num2);
+// if (num1 > num2) {
+//     Console.WriteLine(num1);
+// }
+// else {
+//     Console.WriteLine(num2);
+// }
+
+
+// void randomNumber (){
+// int number = new Random().Next(10,100);
+// // Console.Write(number);
+// int num1 = number / 10;
+// int num2 = number % 10;
+// Console.WriteLine(number + " " + num1 + " " + num2);
+// if (num1 > num2) {
+//     Console.WriteLine(num1);
+// }
+// else {
+//     Console.WriteLine(num2);
+// }
+// }
+// randomNumber();
+
+
+// void randomNumber(int min, int max){
+//     int number = new Random().Next(min,max+1);
+//     Console.Write(number + " -> ");
+//     if (number / 10 > number % 10) {
+//         Console.WriteLine(number / 10);
+//     }
+//     else {
+//         Console.WriteLine(number % 10);
+//     }
+// }
+
+// Console.WriteLine("Введите минимальное число диапазона: ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное число диапазона: ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// randomNumber(start, end);
+
+
+// int SumTwoNumbers( int a, int b){
+//     int sum = a + b;
+//     return sum;
+// }
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Сумма двух чисел: " + SumTwoNumbers(number1, number2));
