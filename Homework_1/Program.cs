ce51e6eaacdb891a38e7f4bd544d75891d9bc699
Asya <fcz777@mail.ru>
2022-06-29 15:01:15 +0300

    Fourth task added

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -42,13 +42,25 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 2 == 1 || number % 2 == -1) {
-    Console.WriteLine("-> нет");
-}
-else {
-    Console.WriteLine("-> да");
-}
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 1 || number % 2 == -1) {
+//     Console.WriteLine("-> нет");
+// }
+// else {
+//     Console.WriteLine("-> да");
+// }
 
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
 
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    if(i % 2 != 1)
+    {
+    Console.WriteLine(i);
+    }
+}
