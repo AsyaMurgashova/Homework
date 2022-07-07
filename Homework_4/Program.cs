5ec29c690e1261937dac75b8bde2e41f6d0c08ff
Asya <fcz777@mail.ru>
2022-07-07 17:55:36 +0300

    One more task added

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -3,11 +3,33 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow(number, degree));
+Console.WriteLine($"{number}, {degree} -> {result}");
+
+
 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+// // int calculations(int N)
+// // {
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int surplus = N % 10;
+// while (surplus >= 9)
+// {
+//     surplus = (N - surplus) / 10;
+// }
+
+//     return surplus;
+// }
+// Console.WriteLine(surplus);
 
 
 
@@ -15,31 +37,31 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] GenerateArray(int N)
-{
-    int[] RandomArray = new int[N];
-    for (int i = 0; i < N; i++)
-    {
-        RandomArray[i] = new Random().Next();
-    }
-    return RandomArray;
-}
+// int[] GenerateArray(int N)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next();
+//     }
+//     return RandomArray;
+// }
 
-void showArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-}
+// void showArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
 
-Console.WriteLine();
-Console.Clear();
+// Console.WriteLine();
+// Console.Clear();
 
-Console.Write("Введите количество элементов массива: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество элементов массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int[] myRandomArray = GenerateArray(num);
-Console.Write($"Массив из {num} случайных элементов -> [ ");
-showArray(myRandomArray);
- Console.WriteLine("]");
+// int[] myRandomArray = GenerateArray(num);
+// Console.Write($"Массив из {num} случайных элементов -> [ ");
+// showArray(myRandomArray);
+// Console.WriteLine("]");
